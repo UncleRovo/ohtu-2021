0561d9d475537ac4114c88797db75b5f7ba25356
Mikkel Leskelä <mikkel.leskela@mail.com>
2021-11-19 21:17:45 +0200

    Tehtävät 1 ja 2

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,7 +1,31 @@
+import requests
+from datetime import datetime
 from player import Player
 
 def main():
-    pass
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players"
+    response = requests.get(url).json()
+
+    players = []
+
+    for player_dict in response:
+        player = Player(
+            player_dict
+        )
+        
+        if player.get("nationality") == "FIN":
+            players.append(player)
+    
+    currentdate = datetime.now()
+    
+    print("Players from FIN: " + str(currentdate))
+    players.sort(key=lambda player: player.getPoints(), reverse=True)
+
+    for player in players:
+        row = player.get("team") +" " + str(player.get("goals")) + " + " + str(player.get("assists")) + " = " + str(player.getPoints())
+        rowjust = 23 - len(player.get("name")) + len(row)
+        
+        print(player.get("name") + row.rjust(rowjust))
 
 if __name__ == "__main__":
     main()
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,9 @@
 class Player:
-    def __init__(self, name):
-        self.name = name
-    
-    def __str__(self):
-        return self.name
+    def __init__(self, playerdict):
+        self.playerdict = playerdict
+        
+    def get(self, value):
+        return self.playerdict[value]
+        
+    def getPoints(self):
+        return self.playerdict["goals"] + self.playerdict["assists"]
